b32284abdc7e6626a3bb5afbae7472d50ea988b8
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2024-07-17 21:03:48 -0500

    V1.0.3: Update: Private DBZ service implemented

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -10,7 +10,7 @@ import { Character } from '../../interfaces/character.interface';
 export class ListComponent {
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
+  public onDelete: EventEmitter<string> = new EventEmitter();
 
 
   @Input()
@@ -21,7 +21,7 @@ export class ListComponent {
     }
   ];
 
-  emitIdCharacter(id: number): void {
+  emitIdCharacter(id: string): void {
     this.onDelete.emit(id);
   }
 
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,6 +1,7 @@
 
 import { Component } from '@angular/core';
-import { type Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,29 +9,18 @@ import { type Character } from '../interfaces/character.interface';
 })
 
 export class MainPageComponent{
-  constructor() { }
 
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 500,
-    },
-    {
-      name: 'Goku',
-      power: 9500,
-    },
-    {
-      name: 'Vegetta',
-      power: 7777,
-    }
-  ];
+  constructor( private dbzService: DbzService) {}
 
-  onNewCharacter( character: Character): void {
-    this.characters.unshift({...character});
+  get characters(): Character[] {
+    return this.dbzService.characters;
   }
 
-  onDeleteIdCharacter( idCharacter: number): void {
-    // this.characters = this.characters.filter(char => char !== idCharacter);
-    this.characters.splice(idCharacter, 1);
+  onDeleteCharacter(id: string): void {
+    this.dbzService.deleteIdCharacter(id);
+  }
+
+  onNewCharacter(character: Character): void {
+    this.dbzService.addCharacter
   }
 }
